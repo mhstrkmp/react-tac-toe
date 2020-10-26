765619b65c263fb4952160c4807ea3cf48d38076
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-26 23:00:12 +0100

    Lifting State Up

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,18 @@
 import "./board.css";
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
   function renderSquare(i) {
-    return <Square value={i} />;
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+      const newSquares = squares.slice();
+      newSquares[i] = "X";
+      setSquares(newSquares);
+    }
+
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
   const status = "Next player: X";
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,10 @@
 import "./square.css";
-import React, { useState } from "react";
+import React from "react";
 
-export default function Square() {
-  const [value, setValue] = useState(null);
+export default function Square(props) {
   return (
-    <button className="square" onClick={() => setValue("X")}>
-      {value}
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
     </button>
   );
 }
